4d56cefed5a73c46dcb6f135290b476529f396c9
Nathalia Prado <nathalia_898@hotmail.com>
2023-08-22 11:54:10 +1200

    add setting up the app component implementation

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,5 +1,16 @@
+import { useState, useEffect } from "react"
+import { Widget } from "../../models/Widget"
+
+const initialWidgets = [] as Widget[]
+
 function App() {
-  return (
+  const [widgets, setWidgets] = useState(initialWidgets)
+
+  useEffect(() => {
+    console.log('using the effect')
+  }, [])
+  
+  return (+
     <div>
       <h1>Widgets for the win!</h1>
     </div>
