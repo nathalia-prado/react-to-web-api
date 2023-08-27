f78f514ba844f0c296bafc0e131aa94023640b47
Nathalia Prado <nathalia_898@hotmail.com>
2023-08-27 18:58:09 +1200

    Consume widget api

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import request from 'superagent'
+import { Widget } from '../models/Widget'
 
 const widgetUrl = '/api/v1/widgets/'
 
-export function getWidgets(): void {}
+export async function getWidgets(): Promise<Widget[]> {
+    const dbWidget = await request.get(widgetUrl)
+    return dbWidget.body
+}
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,18 +1,39 @@
 import { useState, useEffect } from "react"
-import { Widget } from "../../models/Widget"
+import * as MODELS from "../../models/Widget"
+import { getWidgets } from "../apiClient"
 
-const initialWidgets = [] as Widget[]
+const initialWidgets = [] as MODELS.Widget[]
 
 function App() {
   const [widgets, setWidgets] = useState(initialWidgets)
 
   useEffect(() => {
-    console.log('using the effect')
+    try {
+      // eslint-disable-next-line no-inner-declarations
+      async function fetchWidgets() {
+        const dbWidgets = await getWidgets()
+        setWidgets(dbWidgets)
+      }
+      fetchWidgets()
+    } catch(error) {
+      console.log(error)
+    }
   }, [])
   
-  return (+
+  return (
     <div>
       <h1>Widgets for the win!</h1>
+      {widgets.map(widget => {
+          return (
+            <div key={widget.id} className="widget-details">
+              <h2>{widget.name}</h2>
+              <p><b>Price: </b>{widget.price}</p>
+              <p><b>Mfg: </b>{widget.mfg}</p>
+              <p><b>In stock: </b>{widget.inStock}</p>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
